5318a100e7d694e59dadbab9270b5de3a0f2f88b
Shantanu Sharma <shantanu100.sharma@gmail.com>
2026-02-22 17:11:42 +0000

    weight key naming

--- model.py
+++ model.py
@@ -136,9 +136,9 @@ class SiglipAttention(nn.Module):
         self.head_dim = self.embed_dim // self.num_heads
         self.dropout = config.attention_dropout
 
-        self.wq = nn.Linear(self.embed_dim, self.embed_dim, bias=True)
-        self.wk = nn.Linear(self.embed_dim, self.embed_dim, bias=True)
-        self.wv = nn.Linear(self.embed_dim, self.embed_dim, bias=True)
+        self.q_proj = nn.Linear(self.embed_dim, self.embed_dim, bias=True)
+        self.k_proj = nn.Linear(self.embed_dim, self.embed_dim, bias=True)
+        self.v_proj = nn.Linear(self.embed_dim, self.embed_dim, bias=True)
 
         self.out_proj = nn.Linear(self.embed_dim, self.embed_dim)
     
@@ -147,9 +147,9 @@ class SiglipAttention(nn.Module):
         B, num_patches, _ = hidden_states.shape # batch_size x num_patches x embed_dim
 
         # B, num_patches, num_heads * hsize
-        q = self.wq(hidden_states) 
-        k = self.wk(hidden_states)
-        v = self.wv(hidden_states)
+        q = self.q_proj(hidden_states) 
+        k = self.k_proj(hidden_states)
+        v = self.v_proj(hidden_states)
 
         # B, T, num_heads, hsize
         q = q.view(B, num_patches, self.num_heads, self.head_dim)
@@ -179,8 +179,7 @@ class SiglipAttention(nn.Module):
         # B, num_patches, D
         attn = self.out_proj(attn)
 
-        return attn, attn
-
+        return attn, None
 
 class SiglipVisionEmbeddings(nn.Module):
     "Image --> patches using convolutions --> flatten + positional encoding --> image embeddings"
@@ -784,70 +783,3 @@ class PaliGemmaForConditionalGeneration(nn.Module):
         )
 
         return outputs
-
-
-# class SiglipAttention(nn.Module):
-#     """Multi-headed attention from 'Attention Is All You Need' paper"""
-
-#     def __init__(self, config):
-#         super().__init__()
-#         self.config = config
-#         self.embed_dim = config.hidden_size
-#         self.num_heads = config.num_attention_heads
-#         self.head_dim = self.embed_dim // self.num_heads
-#         self.scale = self.head_dim**-0.5 # Equivalent to 1 / sqrt(self.head_dim)
-#         self.dropout = config.attention_dropout
-
-#         self.k_proj = nn.Linear(self.embed_dim, self.embed_dim)
-#         self.v_proj = nn.Linear(self.embed_dim, self.embed_dim)
-#         self.q_proj = nn.Linear(self.embed_dim, self.embed_dim)
-#         self.out_proj = nn.Linear(self.embed_dim, self.embed_dim)
-
-#     def forward(
-#         self,
-#         hidden_states: torch.Tensor,
-#     ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
-
-#         # hidden_states: [Batch_Size, Num_Patches, Embed_Dim]
-#         batch_size, seq_len, _ = hidden_states.size()
-#         # query_states: [Batch_Size, Num_Patches, Embed_Dim]
-#         query_states = self.q_proj(hidden_states)
-#         # key_states: [Batch_Size, Num_Patches, Embed_Dim]
-#         key_states = self.k_proj(hidden_states)
-#         # value_states: [Batch_Size, Num_Patches, Embed_Dim]
-#         value_states = self.v_proj(hidden_states)
-#         # query_states: [Batch_Size, Num_Heads, Num_Patches, Head_Dim]
-#         query_states = query_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
-
-#         key_states = key_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
-
-#         value_states = value_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
-#         # Calculate the attention using the formula Q * K^T / sqrt(d_k). attn_weights: [Batch_Size, Num_Heads, Num_Patches, Num_Patches]
-#         attn_weights = (torch.matmul(query_states, key_states.transpose(2, 3)) * self.scale)
-
-#         if attn_weights.size() != (batch_size, self.num_heads, seq_len, seq_len):
-#             raise ValueError(
-#                 f"Attention weights should be of size {(batch_size, self.num_heads, seq_len, seq_len)}, but is"
-#                 f" {attn_weights.size()}"
-#             )
-
-#         # Apply the softmax row-wise. attn_weights: [Batch_Size, Num_Heads, Num_Patches, Num_Patches]
-#         attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-#         # Apply dropout only during training
-#         attn_weights = nn.functional.dropout(attn_weights, p=self.dropout, training=self.training)
-#         # Multiply the attention weights by the value states. attn_output: [Batch_Size, Num_Heads, Num_Patches, Head_Dim]
-#         attn_output = torch.matmul(attn_weights, value_states)
-
-#         if attn_output.size() != (batch_size, self.num_heads, seq_len, self.head_dim):
-#             raise ValueError(
-#                 f"`attn_output` should be of size {(batch_size, self.num_heads, seq_len, self.head_dim)}, but is"
-#                 f" {attn_output.size()}"
-#             )
-#         # [Batch_Size, Num_Heads, Num_Patches, Head_Dim] -> [Batch_Size, Num_Patches, Num_Heads, Head_Dim]
-#         attn_output = attn_output.transpose(1, 2).contiguous()
-#         # [Batch_Size, Num_Patches, Num_Heads, Head_Dim] -> [Batch_Size, Num_Patches, Embed_Dim]
-#         attn_output = attn_output.reshape(batch_size, seq_len, self.embed_dim)
-#         # [Batch_Size, Num_Patches, Embed_Dim]
-#         attn_output = self.out_proj(attn_output)
-
-#         return attn_output, attn_weights
